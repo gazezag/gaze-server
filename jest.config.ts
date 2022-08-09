7adc1bb7e06c50fd49f0ae755f501641f7c821f7
Ethan Teng <1345118870@qq.com>
2022-08-09 21:33:40 +0800

    sorted out the dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
 import type { InitialOptionsTsJest } from 'ts-jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
 
 const config: InitialOptionsTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node'
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
 
 export default config;
