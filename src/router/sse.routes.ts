0709d7d83f535d3265826b4069270df6ae42d9e7
Ethan Teng <1345118870@qq.com>
2022-08-15 23:21:01 +0800

    fixed the bug in server pushing

--- src/router/sse.routes.ts
+++ src/router/sse.routes.ts
@@ -1,16 +1,35 @@
 import Router from 'koa-router';
 import { store } from '../core/store';
-import { createPusher } from '../core/sse';
+import { PassThrough } from 'stream';
 
 const router = new Router();
 
 router.get('/get-data', async ctx => {
-  setInterval(function () {
+  const stream = new PassThrough();
+
+  // set socket
+  ctx.request.socket.setTimeout(0);
+  ctx.request.socket.setNoDelay(true);
+  ctx.request.socket.setKeepAlive(true);
+
+  // set headers
+  ctx.response.set('Content-Type', 'text/event-stream');
+  ctx.response.set('Cache-Control', 'no-cache');
+  ctx.response.set('Connection', 'keep-alive');
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  ctx.response.status = 200;
+  ctx.response.body = stream;
+
+  //! timer is required here, and I don't know why
+  const timer = setInterval(() => {
     if (!store.isEmpty()) {
-      // TODO
-      createPusher(ctx)(store.dequeue()!);
+      stream.write(store.dequeue());
     }
-  }, 1000);
+  }, 500);
+
+  stream.on('close', () => {
+    clearInterval(timer);
+  });
 });
 
 export default router;
