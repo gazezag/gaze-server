ee4ea5649f06647f37163d80d59646849f2177a4
Ethan Teng <1345118870@qq.com>
2022-08-15 23:12:27 +0800

    added some static enumeration

--- src/config/static.ts
+++ src/config/static.ts
@@ -1,12 +1,24 @@
 export enum TableName {
   index = 'index',
   deviceInfo = 'device_info',
-  pageInfo = 'page_info'
+  pageInfo = 'page_info',
+  performanceTiming = 'performance_timing',
+  navigationTiming = 'navigation_timing',
+  resourceFlow = 'resource_flow',
+  routerChange = 'router_change',
+  operation = 'operation',
+  http = 'http'
   // TODO
 }
 
 export enum ServerSendEventName {
   deviceInfo = 'device-info',
-  pageInfo = 'page-info'
+  pageInfo = 'page-info',
+  performanceTiming = 'performance-timing',
+  navigationTiming = 'navigation-timing',
+  resourceFlow = 'resource-flow',
+  routerChange = 'router-change',
+  operation = 'operation',
+  http = 'http'
   // TODO
 }
