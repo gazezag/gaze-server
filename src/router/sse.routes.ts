25e8868542f1038bd646a44c6de2511316eba5e9
Ethan Teng <1345118870@qq.com>
2022-09-06 19:38:01 +0800

    refactored SSE Server with Tramsform stream

--- src/router/sse.routes.ts
+++ src/router/sse.routes.ts
@@ -1,16 +1,15 @@
 import Router from 'koa-router';
 import { store } from '../core/store';
-import { PassThrough } from 'stream';
+import { Transform } from 'stream';
 
 const router = new Router();
 
 router.get('/get-data', async ctx => {
-  const stream = new PassThrough();
-
-  // set socket
-  ctx.request.socket.setTimeout(0);
-  ctx.request.socket.setNoDelay(true);
-  ctx.request.socket.setKeepAlive(true);
+  const stream = new Transform();
+  stream._transform = (msg, _, cb) => {
+    stream.push(msg);
+    cb();
+  };
 
   // set headers
   ctx.response.set('Content-Type', 'text/event-stream');
@@ -20,7 +19,6 @@ router.get('/get-data', async ctx => {
   ctx.response.status = 200;
   ctx.response.body = stream;
 
-  //! timer is required here, and I don't know why
   const timer = setInterval(() => {
     if (!store.isEmpty()) {
       stream.write(store.dequeue());
@@ -31,5 +29,32 @@ router.get('/get-data', async ctx => {
     clearInterval(timer);
   });
 });
+// router.get('/get-data', async ctx => {
+//   const stream = new PassThrough();
+
+//   // set socket
+//   ctx.request.socket.setTimeout(0);
+//   ctx.request.socket.setNoDelay(true);
+//   ctx.request.socket.setKeepAlive(true);
+
+//   // set headers
+//   ctx.response.set('Content-Type', 'text/event-stream');
+//   ctx.response.set('Cache-Control', 'no-cache');
+//   ctx.response.set('Connection', 'keep-alive');
+//   ctx.response.set('Access-Control-Allow-Origin', '*');
+//   ctx.response.status = 200;
+//   ctx.response.body = stream;
+
+//   //! timer is required here, and I don't know why
+//   const timer = setInterval(() => {
+//     if (!store.isEmpty()) {
+//       stream.write(store.dequeue());
+//     }
+//   }, 100);
+
+//   stream.on('close', () => {
+//     clearInterval(timer);
+//   });
+// });
 
 export default router;
